f6d633511a8487afa420d18840f51947162c8307
agungwe <agungw@ecampus.ut.ac.id>
2020-08-06 08:41:17 +0700

    Upload Async

---
+++ 003.js
@@ -0,0 +1,16 @@
+const angkafunc = () => {
+	return new Promise((resolve, reject) => {
+		for (let i = 1; i <= 3; i++) {
+		  setTimeout(() => {
+		      console.log(i);
+		      if( i === 3 ){
+		      	resolve();
+		      }
+		  }, 1000);
+		}
+	});
+}
+
+angkafunc().then(function() {
+  console.log('Done');
+});
